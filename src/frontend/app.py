051391a9eb9fdc42de112702ebaf3748b49a660f
gersongerardcruz <ggerardcruz@gmail.com>
2023-03-01 09:43:56 +0800

    initial image reco with session state

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -3,82 +3,93 @@ import streamlit as st
 import requests
 import pandas as pd
 import json
+import sqlite3
 from PIL import Image
-      
-# Define function to make API request and return predictions
-def get_predictions(file_bytes):
 
-    # Define the FastAPI endpoint URL
-    api_url = args.endpoint
+button1 = st.button('Check 1')
 
-    # Make API request
-    response = requests.post(api_url, files={'file': file_bytes})
+if st.session_state.get('button') != True:
 
-    # Check if request was successful
-    if response.status_code == 200:
-        # Parse predictions from response
-        predictions = response.json()
+    st.session_state['button'] = button1
 
-        # Return predictions
-        return predictions
+# Set app title
+st.title('Animal Classifier')
 
-    # If request failed, raise an exception
-    else:
-        raise Exception('API request failed with status code {}'.format(response.status_code))
+# Define a placeholder for the uploaded file data
+image = None
 
-# Define the Streamlit app
-def app():
+# Define a placeholder for the predictions
+predictions = None
 
-    # Set app title
-    st.title('Animal Classifier')
+# Use st.file_uploader to get a file from the user
+st.caption("Choose an image for classification here")
+file = st.file_uploader('Upload image', type=['jpg', 'jpeg', 'png'], label_visibility='visible')
 
-    # Define a placeholder for the uploaded file data
-    image = None
+# If a file was uploaded
+if file is not None:
 
-    # Define a placeholder for the predictions
-    predictions = None
+    # Convert image to bytes
+    image_bytes = file.read()
 
-    # Use st.file_uploader to get a file from the user
-    st.caption("Choose an image for classification here")
-    file = st.file_uploader('Upload image', type=['jpg', 'jpeg', 'png'], label_visibility='visible')
-
-    # If a file was uploaded
-    if file is not None:
-
-        # Convert image to bytes
-        image_bytes = file.read()
-
-        # Display the uploaded image
-        image = Image.open(file)
-        st.image(image, caption='Uploaded Image')
+    # Display the uploaded image
+    image = Image.open(file)
+    st.image(image, caption='Uploaded Image')
 
 
-    # If the user has uploaded an image and wants to make predictions
-    if image is not None and st.button('Predict'):
+# If the user has uploaded an image and wants to make predictions
+if image is not None and st.session_state['button'] == True:
+    print("Button1")
+    st.caption("Prediction in progress...")
+    # Use st.spinner to display a spinner while predictions are being made
+    with st.spinner('Predicting...'):
+        # Define the FastAPI endpoint URL
+        api_url = 'http://localhost:8000/predict'
 
-        st.caption("Prediction in progress...")
-        # Use st.spinner to display a spinner while predictions are being made
-        with st.spinner('Predicting...'):
-            # Make API request to get predictions
-            predictions = get_predictions(image_bytes)
+        # Make API request
+        response = requests.post(api_url, files={'file': image_bytes})
 
-        # Display the prediction results
-        st.subheader('That animal is a ', predictions["response"])
+        # Check if request was successful
+        if response.status_code == 200:
+            # Parse predictions from response
+            predictions = response.json()
 
-    # If predictions have been made, display a download button for the predictions
-    if predictions is not None:
-        # Use st.download_button to allow the user to download the predictions as a CSV file
-        st.download_button(
-            label='Download predictions',
-            data=json.dumps(predictions),
-            file_name='predictions.json',
-            mime='application/json'
-        )
+        # If request failed, raise an exception
+        else:
+            raise Exception('API request failed with status code {}'.format(response.status_code))
+    
+    label = predictions["response"]
+    # Display the prediction results
+    st.subheader(f'That animal is a {label}')
 
-# Run the Streamlit app
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--endpoint", help="endpoint for prediction", default='http://localhost:8000/predict')
-    args = parser.parse_args()
+    if st.button('Check 2'):
+            print("Clicked")
+            st.caption("Recommending images...")
+            
+            # Connect to the SQLite database
+            conn = sqlite3.connect("databases/images.db")
 
-    app()
+            cursor = conn.cursor()
+            # Get a list of 5 random images from the database
+            query = f"SELECT file_path FROM metadata WHERE label='{label}' ORDER BY RANDOM() LIMIT 5"
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            images = [row[0] for row in rows]
+
+            # Display the images using Streamlit
+            for img_path in images:
+                image = Image.open(img_path)
+                st.image(image, caption=img_path)
+
+            conn.close()
+
+            st.session_state['button'] = False
+
+# # If predictions have been made, display a download button for the predictions
+# if predictions is not None:
+#     # Use st.download_button to allow the user to download the predictions as a CSV file
+#     st.download_button(
+#         label='Download predictions',
+#         data=json.dumps(predictions),
+#         file_name='predictions.json',
+#         mime='application/json'
+#     )
